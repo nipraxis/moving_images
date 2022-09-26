ca5b4efea2c0a93d4ff4a36ca7f8c8e47c8b1df9
Matthew Brett <matthew.brett@gmail.com>
2022-09-26 16:18:30 +0100

    Update from template

---
+++ make_moved.py
@@ -0,0 +1,20 @@
+""" Make image to move
+"""
+
+import numpy as np
+import scipy.ndimage as snd
+
+import nibabel as nib
+
+import nipraxis
+
+anat_fname = nipraxis.fetch_file('ds114_sub009_highres.nii')
+img = nib.load(anat_fname)
+data = img.get_fdata()
+
+orig_slice = data[:, :, 160]
+
+new_slice = snd.shift(orig_slice, [29, -5])
+
+np.savetxt('original_slice.txt', orig_slice)
+np.savetxt('moved_slice.txt', new_slice)
